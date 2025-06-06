ea0f267bcb5eccfa46b86119acefa5ad69984e67
Frederick Kwaku Kankam <fkwekukankam@gmail.com>
2025-06-06 19:53:24 +0000

    Add create and retrieve functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
+from models import supplier_pydantic, supplier_pydanticIn, Supplier
 
 
 app = FastAPI()
@@ -7,6 +8,19 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"message": "Hello, World!"}
+
+@app.post("/supplier")
+async def add_supplier(supplier_info: supplier_pydanticIn):
+    supplier_object = await Supplier.create(**supplier_info.dict())
+    response = await supplier_pydantic.from_tortoise_orm(supplier_object)
+    return {"status": "ok", "data": response}
+
+
+@app.get("/supplier")
+async def get_all_suppliers():
+    response = await supplier_pydantic.from_queryset(Supplier.all())
+    return {"status": "ok", "data": response}
+
 
 # Register Tortoise ORM with FastAPI
 register_tortoise(
