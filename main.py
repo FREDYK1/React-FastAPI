54f0e940f3df89049e3abcb3ef94b02f991d3a67
FREDYK1 <fkwekukankam@gmail.com>
2025-06-07 19:04:43 +0000

    create update and delete functionalities

--- main.py
+++ main.py
@@ -26,6 +26,23 @@ async def get_specific_supplier(supplier_id: int):
     response = await supplier_pydantic.from_queryset_single(Supplier.get(id=supplier_id))
     return {"status": "ok", "data": response}
 
+@app.put("/supplier/{supplier_id}")
+async def update_supplier(supplier_id: int, update_info: supplier_pydanticIn):
+    supplier = await Supplier.get(id=supplier_id)
+    update_info = update_info.dict(exclude_unset=True)
+    supplier.name = update_info["name"]
+    supplier.company = update_info["company_name"]
+    supplier.phone = update_info["phone"]
+    supplier.email = update_info["email"]
+    await supplier.save()
+    response = await supplier_pydantic.from_tortoise_orm(supplier)
+    return {"status": "ok", "data": response}
+
+@app.delete("/supplier/{supplier_id}")
+async def delete_supplier(supplier_id: int):
+    supplier = await Supplier.get(id=supplier_id)
+    await supplier.delete()
+    return {"status": "ok", "message": "Supplier deleted successfully."}
 
 # Register Tortoise ORM with FastAPI
 register_tortoise(
