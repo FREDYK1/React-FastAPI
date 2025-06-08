ee7330aed059c241de52ccc5090b9981eed79945
FREDYK1 <fkwekukankam@gmail.com>
2025-06-08 00:36:32 +0000

    Apply CRUD functionality to the products model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
-from models import supplier_pydantic, supplier_pydanticIn, Supplier
+from models import (supplier_pydantic, supplier_pydanticIn, Supplier,
+                    product_pydanticIn, product_pydantic, Product)
 
 
 app = FastAPI()
@@ -43,6 +44,38 @@ async def delete_supplier(supplier_id: int):
     supplier = await Supplier.get(id=supplier_id)
     await supplier.delete()
     return {"status": "ok", "message": "Supplier deleted successfully."}
+
+@app.post("/product/{supplier_id}")
+async def add_product(supplier_id: int, product_details: product_pydanticIn):
+    supplier = await Supplier.get(id=supplier_id)
+    product = product_details.dict(exclude_none=True)
+    product["revenue"] += product["unit_price"] * product["quantity_sold"]
+    product_obj = await Product.create(**product, supplied_by=supplier)
+    response = await product_pydantic.from_tortoise_orm(product_obj)
+    return {"status": "ok", "data": response}
+
+@app.get("/product")
+async def get_all_products():
+    response =  await product_pydantic.from_queryset(Product.all())
+    return {"status": "ok", "data": response}
+
+@app.get("/product/{product_id}")
+async def get_specific_product(product_id: int):
+    response = await product_pydantic.from_queryset_single(Product.get(id=product_id))
+    return {"status": "ok", "data": response}
+
+@app.put("/product/{product_id}")
+async def update_product(product_id: int, update_info: product_pydanticIn):
+    old_product = await Product.get(id=product_id)
+    update_info = update_info.dict(exclude_unset=True)
+    old_product.name = update_info["name"]
+    old_product.quantity_in_stock = update_info["quantity_in_stock"]
+    old_product.quantity_sold = update_info["quantity_sold"]
+    old_product.unit_price = update_info["unit_price"]
+    old_product.revenue += update_info["unit_price"] * update_info["quantity_sold"]
+    await old_product.save()
+    response = await product_pydantic.from_tortoise_orm(old_product)
+    return {"status": "ok", "data": response}
 
 # Register Tortoise ORM with FastAPI
 register_tortoise(
