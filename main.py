eb6bc20f67bd82ac1acf48276d8edc00dc593671
FREDYK1 <fkwekukankam@gmail.com>
2025-06-08 02:20:51 +0000

    Send email functionality

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks, UploadFile, File, Form
 from tortoise.contrib.fastapi import register_tortoise
 from models import (supplier_pydantic, supplier_pydanticIn, Supplier,
                     product_pydanticIn, product_pydantic, Product)
+from starlette.responses import JSONResponse
+from starlette.requests import Request
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from typing import List
+from pydantic import BaseModel, EmailStr
+from dotenv import dotenv_values
 
+
+credentials = dotenv_values(".env")
 
 app = FastAPI()
 
@@ -70,12 +78,61 @@ async def update_product(product_id: int, update_info: product_pydanticIn):
     update_info = update_info.dict(exclude_unset=True)
     old_product.name = update_info["name"]
     old_product.quantity_in_stock = update_info["quantity_in_stock"]
-    old_product.quantity_sold = update_info["quantity_sold"]
+    old_product.quantity_sold += update_info["quantity_sold"]
     old_product.unit_price = update_info["unit_price"]
     old_product.revenue += update_info["unit_price"] * update_info["quantity_sold"]
     await old_product.save()
     response = await product_pydantic.from_tortoise_orm(old_product)
     return {"status": "ok", "data": response}
+
+@app.delete("/product/{product_id}")
+async def delete_product(product_id: int):
+    product = await Product.get(id=product_id)
+    await product.delete()
+    return {"status": "ok", "message": "Product deleted successfully."}
+
+
+class EmailSchema(BaseModel):
+    email: List[EmailStr]
+
+class EmailContent(BaseModel):
+    subject: str
+    body: str
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=credentials["EMAIL"],
+    MAIL_PASSWORD=credentials["PASSWORD"],
+    MAIL_FROM=credentials["EMAIL"],
+    MAIL_PORT=465,
+    MAIL_SERVER="smtp.gmail.com",
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+)
+
+@app.post("/email/{product_id}")
+async def send_email(product_id: int, content: EmailContent):
+    product = await Product.get(id=product_id)
+    supplier = await product.supplied_by
+    supplier_email = [supplier.email]
+
+    html = """
+    <h5>Thanks for using Fastapi-mail</h5> 
+    <br> 
+    <p>{content.body}</p>
+    <br>
+    <h3>Best Regards</h3>
+    """
+    message = MessageSchema(
+        subject=content.subject,
+        recipients=supplier_email,
+        body=html,
+        subtype="html"
+    )
+
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    return JSONResponse(status_code=200, content={"message": "email has been sent"})
 
 # Register Tortoise ORM with FastAPI
 register_tortoise(
