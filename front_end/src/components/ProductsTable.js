c644a3c64f8143298cd52c0d646b856a772dc7f0
FREDYK1 <fkwekukankam@gmail.com>
2025-06-12 22:42:02 +0000

    Connecting to API

--- front_end/src/components/ProductsTable.js
+++ front_end/src/components/ProductsTable.js
@@ -1,4 +1,4 @@
-import react, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { Table } from 'react-bootstrap';
 import { ProductContext } from '../ProductContext';
 
@@ -6,12 +6,15 @@ const ProductsTable = () => {
     const { products, setProducts } = useContext(ProductContext);
 
     useEffect(() => {
-    fetch("http://127.0.0.1:8000/products")
-    .then(response => { response.json(); })
-    .then(results => { console.log(results)
-    setProducts({data: [...results.data]})
-    })
-    })
+        fetch("http://127.0.0.1:8000/products")
+            .then(response => response.json())
+            .then(data => {
+                setProducts(Array.isArray(data) ? data : []);
+            })
+            .catch(error => {
+                console.error("Failed to fetch:", error);
+            });
+    }, [setProducts])
 
     return (
         <Table striped bordered hover>
@@ -26,9 +29,20 @@ const ProductsTable = () => {
                     <th>Revenue</th>
                     <th>Actions</th>
                 </tr>
-                    <tbody>
-                    </tbody>
             </thead>
+            <tbody>
+                {products && products.map(product => (
+                    <tr key={product.id}>
+                        <td>{product.id}</td>
+                        <td>{product.name}</td>
+                        <td>{product.quantity_in_stock}</td>
+                        <td>{product.quantity_price}</td>
+                        <td>{product.quantity_sold}</td>
+                        <td>{product.unit_price}</td>
+                        <td>{product.revenue}</td>
+                    </tr>
+                ))}
+            </tbody>
         </Table>
     );
 }
